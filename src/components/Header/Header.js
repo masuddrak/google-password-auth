570c74d172b15797760d5d8f7d6a90f500559d6c
masud rana <masudranaacb@gmail.com>
2022-04-11 14:26:13 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <a href="/home">Home</a>
+            <a href="/about">about</a>
+        </div>
+    );
+};
+
+export default Header;
